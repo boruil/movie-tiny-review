d8464bb07faf4da821383d5179bdda8732d09493
Andy <boruil@cs.washington.edu>
2014-08-17 12:17:03 -0700

    add pages port

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ app.set('view engine', 'jade')
 app.listen(port)
 
 console.log('service launched' + port)
+
+// index page
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'movie-tiny-review--Home'
+	})
+})
+
+// detail page
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'movie-tiny-review--Detail'
+	})
+})
+
+// admin page
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'movie-tiny-review--Admin'
+	})
+})
+
+// list page
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'movie-tiny-review--List'
+	})
+})
